fabe9b38a50fb143de281a0904d41d5f68acd424
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-11-13 20:00:28 -0500

    Adding layout and routes for programs page

--- portfolio-app/app/router.js
+++ portfolio-app/app/router.js
@@ -7,7 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('programs');
+  this.route('programs', function() {
+    this.route('swift');
+    this.route('c');
+    this.route('java');
+  });
   this.route('projects');
   this.route('about');
 });
