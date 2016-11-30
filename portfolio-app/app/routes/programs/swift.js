3bcbfb62ba1eea27afbb81b7c71b8c9ee99f000c
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-11-30 17:00:59 -0500

    Adding cards to swift view

--- portfolio-app/app/routes/programs/swift.js
+++ portfolio-app/app/routes/programs/swift.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller) {
+    console.log(controller.toString());
+    controller.setupController();
+  }
 });
