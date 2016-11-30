b34400cc10497b72eadb14fbcaa1da83bb9fbdc7
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-11-30 17:11:24 -0500

    Adding action title and link

--- portfolio-app/app/controllers/programs/swift.js
+++ portfolio-app/app/controllers/programs/swift.js
@@ -9,6 +9,7 @@ export default Ember.Controller.extend({
         title: 'Cycles',
         image: '../assets/images/Cycles-AW42.png',
         id: 'cycles',
+        linkTitle: 'Go To Cycles Page',
         link: 'http://www.getcyclesapp.co',
         content: 'Take control of your life with Cycles, the daily goal motivator for Apple Watch and iPhone! Cycles allows you to easily and efficiently stay on track with your daily goals.'
       },
@@ -16,6 +17,7 @@ export default Ember.Controller.extend({
         title: 'SwiftyCode',
         image: '../assets/images/SwiftyCode-iOS.png',
         id: 'swiftycode',
+        linkTitle: 'View in Github',
         link: '', //TODO: Add swiftyCode to projects repo //
         content: 'Collborate on blocks of code using SwiftyCode through iMessage for iPhone'
       },
@@ -23,6 +25,7 @@ export default Ember.Controller.extend({
         title: 'The Forest',
         image: '../assets/images/TheForest-AW42.png',
         id: 'theforest',
+        linkTitle: 'View in Github',
         link: 'https://github.com/adilanchian/UHack',
         content: 'Choose your own adventure game, that takes you through multiple scenarios where you decide the outcome! Developed for Apple Watch using Swift' 
       },
@@ -30,6 +33,7 @@ export default Ember.Controller.extend({
         title: 'Age',
         image: '../assets/images/Age-iOS.png',
         id: 'age',
+        linkTitle: 'View in Github',
         link: 'https://github.com/adilanchian/Projects/tree/master/programs-in-swift/Age',
         content: 'Every wonder what your age is in dog years? Age is a simple app that does just that'
       },
@@ -37,6 +41,7 @@ export default Ember.Controller.extend({
         title: 'Guessing Game',
         image: '../assets/images/GuessingGame-iOS.png',
         id: 'guessing-game',
+        linkTitle: 'View in Github',
         link: 'https://github.com/adilanchian/Projects/tree/master/programs-in-swift/Guessing%20Game',
         content: 'Guessing game is a simple game that challenges your luck by having you guess how many "digital fingers" are being held up d'
       }
